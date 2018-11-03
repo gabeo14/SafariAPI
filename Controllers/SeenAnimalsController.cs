68bf44475657f5d8521558ca225209ba5e527bee
Gabe Oleinik <gomusic14@gmail.com>
2018-11-03 13:46:22 -0400

    put working

--- Controllers/SeenAnimalsController.cs
+++ Controllers/SeenAnimalsController.cs
@@ -28,5 +28,33 @@ namespace SafariAPI.Controllers
       db.SaveChanges();
       return seenAnimals;
     }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+      var db = new SafariAPIContext();
+      var animal = db.SeenAnimals.FirstOrDefault(a => a.Id == id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      db.SeenAnimals.Remove(animal);
+      db.SaveChanges();
+      return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<SeenAnimals> Put([FromRoute] int id, [FromBody] SeenAnimals updated)
+    {
+      var db = new SafariAPIContext();
+      var seenAnimals = db.SeenAnimals.FirstOrDefault(a => a.Id == id);
+
+      seenAnimals.Species = updated.Species;
+      seenAnimals.CountOfTimesSeen = updated.CountOfTimesSeen;
+      seenAnimals.LocationOfLastSeen = updated.LocationOfLastSeen;
+
+      db.SaveChanges();
+      return updated;
+    }
   }
 }
